792f07b0c867b6649496374161c2cbaaef8d62cd
Philip <filipposbatistatos@gmail.com>
2021-02-18 07:10:49 +1100

    Image and video rescalling

--- reading/readingImages.py
+++ reading/readingImages.py
@@ -2,8 +2,16 @@
 #then resize the image to fit in your screen
 import cv2 as cv
 
+def rescaleFrame(frame, scale=0.25):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
 img = cv.imread('dog-one.jpg')
 
-cv.imshow('Cat', img)
+cv.imshow('Big Dog', img)
+cv.imshow('Small Dog', rescaleFrame(img))
 
 cv.waitKey(0)
--- reading/readingVideo.py
+++ reading/readingVideo.py
@@ -3,9 +3,20 @@ import cv2 as cv
 capture = cv.VideoCapture(0) #this refers to camera
 #if you want to use a file, provide the path
 
+#this can also be achieved using cv.set
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
 while True:
     isTrue, frame = capture.read()
     cv.imshow('Video', frame)
+
+    frame_resized = rescaleFrame(frame)
+    cv.imshow('Small Video', frame)
 
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
