d3d6ae999d8b8ac7901eda2f9a13479a20ca1069
Philip <filipposbatistatos@gmail.com>
2021-02-19 06:04:41 +1100

    Image translformation

---
+++ basics/imageTransformations.py
@@ -0,0 +1,46 @@
+import cv2 as cv
+import numpy as np
+
+def rescaleFrame(frame, scale=0.25):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+img = rescaleFrame(cv.imread('dog-one.jpg'))
+
+# Translation
+def translate(img, x, y):
+    transMat = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+
+translated = translate(img, 100, 100)
+cv.imshow('Translated', translated)
+
+# Rotation
+def rotate(img, angle, rotPoint=None):
+    (height,width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, 45)
+cv.imshow('Rotated', rotated)  
+
+# Resizing
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+# Flipping
+#0-1 horizontal flipping
+#+ - vertical flipping
+flip = cv.flip(img, -1)
+cv.imshow('Flip', flip)
+
+cv.waitKey(0)
