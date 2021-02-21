9c6c933f6ecd346ea43320256cd35012736625a5
Philip <filipposbatistatos@gmail.com>
2021-02-22 06:20:13 +1100

    Histograms

--- Advanced/masking.py
+++ Advanced/masking.py
@@ -9,6 +9,7 @@ blank = np.zeros(img.shape[:2], dtype='uint8')
 mask = cv.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
 cv.imshow('Mask', mask)
 
+#the size of the mask must be the same as the size of the image. s
 masked = cv.bitwise_and(img, img, mask=mask)
 cv.imshow('Masked', masked)
 
